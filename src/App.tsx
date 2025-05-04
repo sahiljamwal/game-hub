126a4a940605909d9bcf2215fafad640a72b64a3
sahiljamwal <sahil.jamwal78625@gmail.com>
2025-05-04 09:38:48 +0530

    chore(main): Added responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <Button>Click me</Button>;
+  return (
+    <Grid
+      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="dodgerblue"></GridItem>
+    </Grid>
+  );
 }
 
 export default App;
