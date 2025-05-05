ed35719708da3d11b096ef23e0764b747d48fb82
sahiljamwal <sahil.jamwal78625@gmail.com>
2025-05-05 07:12:19 +0530

    chore(main): Added genres list component

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,26 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
 
   return (
-    <ul>
+    <List>
       {data?.length > 0 &&
-        data.map((genre) => <li key={genre.id}>{genre.name}</li>)}
-    </ul>
+        data.map((genre) => (
+          <ListItem key={genre.id} paddingY={"5px"}>
+            <HStack>
+              <Image
+                boxSize={"32px"}
+                borderRadius={8}
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              <Text fontSize={"lg"}>{genre.name}</Text>
+            </HStack>
+          </ListItem>
+        ))}
+    </List>
   );
 };
 
