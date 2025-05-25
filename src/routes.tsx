5a190610c0ff90e262aabfdf8d5f61066d2d0ccd
sahiljamwal <sahil.jamwal78625@gmail.com>
2025-05-25 08:44:46 +0530

    feat(routes): Add ErrorPage component for handling route errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import GameDetails from "./pages/GameDetails";
 import Layout from "./pages/Layout";
 import Homepage from "./pages/Homepage";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
